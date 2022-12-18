f6a94011da0e50a5ee884e28ee7fdb67c4ba46fd
Michael Uhlenberg <michael.uhlenberg@t-online.de>
2022-12-18 15:28:47 +0100

    course contents removed

--- src/routes/about/+page.js
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-
-/** @type {import('./$types').PageLoad} */
-export function load({ params }) {
-	return {
-		title: 'Hello world!',
-		content: '<h2>Welcome to our blog. Lorem ipsum dolor sit amet...</h2>'
-	};
-}
--- src/routes/posts/[slug]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	/** @type {import('./$types').PageData} */
-	export let data;
-</script>
-
-<p class="font-ueberschrift">TestSlug</p>
-<h1>{data.title}</h1>
-<div>{@html data.content}</div>
-<p>{data.slug}</p>
